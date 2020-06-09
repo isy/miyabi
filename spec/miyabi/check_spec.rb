accff37be99fc37942de80c13e2ad2f45736b435
kazumalab <kazuma-arashi@hotmail.co.jp>
2020-06-09 10:43:22 +0900

    add: check and format test

---
+++ spec/miyabi/check_spec.rb
@@ -0,0 +1,149 @@
+require "spec_helper"
+
+RSpec.describe "check" do
+  describe "#is_hira?" do
+    subject { text.is_hira? }
+
+    let(:text) { "ひらがな" }
+
+    context "when hiragana" do
+      it { is_expected.to be_truthy }
+    end
+
+    context "when romaji" do
+      let(:text) { "hiragana" }
+
+      it { is_expected.to be_falsey }
+    end
+
+    context "when not hiragana" do
+      let(:text) { "カタカナ" }
+
+      it { is_expected.to be_falsey }
+    end
+
+    context "when includes hiragana" do
+      let(:text) { "ひらがなカタカナ" }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context "when hiragana with emoji" do
+      let(:text) { "😉ひらがな" }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context "when hiragana with special charactor" do
+      let(:text) { "𓇼𖠋𓆫𓀤𓆉𓆡ひらがな" }
+
+      it { is_expected.to be_truthy }
+    end
+  end
+
+  describe "#is_kana?" do
+    subject { text.is_kana? }
+
+    let(:text) { "カタカナ" }
+
+    context "when katakana" do
+      it { is_expected.to be_truthy }
+    end
+
+    context "when hiragana" do
+      let(:text) { "ひらがな" }
+
+      it { is_expected.to be_falsey }
+    end
+
+    context "when romaji" do
+      let(:text) { "katakana" }
+
+      it { is_expected.to be_falsey }
+    end
+
+    context "when includes katakana" do
+      let(:text) { "ひらがなカタカナ" }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context "when katakana with emoji" do
+      let(:text) { "😉カタカナ" }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context "when hiragana with special charactor" do
+      let(:text) { "𓇼𖠋𓆫𓀤𓆉𓆡カタカナ" }
+
+      it { is_expected.to be_truthy }
+    end
+  end
+
+  describe "#is_kanji?" do
+    subject { text.is_kanji? }
+
+    let(:text) { "漢字" }
+
+    context "when kanji" do
+      it { is_expected.to be_truthy }
+    end
+
+    context "when hiragana" do
+      let(:text) { "ひらがな" }
+
+      it { is_expected.to be_falsey }
+    end
+
+    context "when kanji with hiragana" do
+      let(:text) { "明日は晴れ" }
+
+      it { is_expected.to be_falsey }
+    end
+
+    context "when kanji with katanaka" do
+      let(:text) { "快晴🌞" }
+
+      it { is_expected.to be_falsey }
+    end
+  end
+
+  describe "#is_japanese?" do
+    subject { text.is_japanese? }
+
+    let(:text) { "明日は晴れ" }
+
+    context "when japanese" do
+      it { is_expected.to be_truthy }
+    end
+
+    context "when japanese with ！" do
+      let(:text) { "明日は晴れ！" }
+
+      it { is_expected.to be_falsey }
+    end
+  end
+
+  describe "#is_roman?" do
+    subject { text.is_roman? }
+
+    let(:text) { "romaji" }
+
+    context "when romaji" do
+      it { is_expected.to be_truthy }
+    end
+
+    context "when hiragana" do
+      let(:text) { "ひらがな" }
+
+      it { is_expected.to be_falsey }
+    end
+
+    context "when katakana" do
+      let(:text) { "カタカナ" }
+
+      it { is_expected.to be_falsey }
+    end
+  end
+end
---
+++ spec/miyabi/format_spec.rb
@@ -0,0 +1,105 @@
+require "spec_helper"
+
+RSpec.describe "format" do
+  describe "#to_kana" do
+    subject { text.to_kana }
+
+    let(:text) { "ひらがな" }
+
+    context "when hiragana" do
+      it { is_expected.to eq "ヒラガナ" }
+    end
+
+    context "when katakana" do
+      let(:text) { "カタカナ" }
+
+      it { is_expected.to eq "カタカナ" }
+    end
+
+    context "when hiragana with katakana" do
+      let(:text) { "ひらがなカタカナ" }
+
+      it { is_expected.to eq "ヒラガナカタカナ" }
+    end
+
+    context "when romaji" do
+      let(:text) { "romaji" }
+
+      it { is_expected.to eq "romaji" }
+    end
+
+    context "when kanji" do
+      let(:text) { "漢字" }
+
+      it { is_expected.to eq "漢字" }
+    end
+  end
+
+  describe "#to_hira" do
+    subject { text.to_hira }
+
+    let(:text) { "カタカナ" }
+
+    context "when katakana" do
+      it { is_expected.to eq "かたかな" }
+    end
+
+    context "when hiragana" do
+      let(:text) { "ひらがな" }
+
+      it { is_expected.to eq "ひらがな" }
+    end
+
+    context "when katakana with hiragana" do
+      let(:text) { "カタカナひらがな" }
+
+      it { is_expected.to eq "かたかなひらがな" }
+    end
+
+    context "when katakana with kanji" do
+      let(:text) { "カタカナ日本語" }
+
+      it { is_expected.to eq "かたかな日本語" }
+    end
+  end
+
+  describe "#to_roman" do
+    subject { text.to_roman }
+
+    let(:text) { "ひらがな" }
+
+    context "when hiragana" do
+      it { is_expected.to eq "hiragana" }
+    end
+
+    context "when katakana" do
+      let(:text) { "カタカナ" }
+
+      it { is_expected.to eq "katakana" }
+    end
+
+    context "when hiragana with katakana" do
+      let(:text) { "ひらがなカタカナ" }
+
+      it { is_expected.to eq "hiraganakatakana" }
+    end
+
+    context "when hiragana with kanji" do
+      let(:text) { "ひらがな漢字" }
+
+      it { is_expected.to eq "hiragana" }
+    end
+
+    context "when hiragana with romaji and kanji" do
+      let(:text) { "ひらがな漢字mix" }
+
+      it { is_expected.to eq "hiragana" }
+    end
+
+    context "when hiragana with special character" do
+      let(:text) { "ひらがな！" }
+
+      it { is_expected.to eq "hiragana" }
+    end
+  end
+end
